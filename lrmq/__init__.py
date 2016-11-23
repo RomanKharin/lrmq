86ed5eb726e47180524047f6d54130fd123404cd
Roman Kharin <romiq.kh@gmail.com>
2016-11-23 20:34:42 +0300

    Command line: add --agent-log option

--- lrmq/__init__.py
+++ lrmq/__init__.py
@@ -50,6 +50,8 @@ def main(cfg = None):
         parser.add_argument("-a", "--agent", nargs = argparse.REMAINDER,
             help = "Start agent with hub. "\
             "Remainder is command line for new process")
+        parser.add_argument("-al", "--agent-log",
+            help = "Agent log filename (default stdout, with --agent only)")
         parser.add_argument("-all", "--agent-loglevel",
             help = "Agent log level (with --agent only)")
         args = parser.parse_args()
@@ -89,6 +91,8 @@ def main(cfg = None):
                     acfg["loglevel"] = int(args.agent_loglevel, 10)
                 except ValueError:
                     acfg["loglevel"] = args.agent_loglevel
+            if args.agent_log:
+                acfg["log"] = args.agent_log 
             cfg["agents"] = [acfg]
 
     hub = Hub()
